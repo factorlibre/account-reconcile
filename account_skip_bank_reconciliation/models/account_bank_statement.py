31c521a56b030456b3253347e4964655d190df68
Hugo Santos <hugo.santos@factorlibre.com>
2022-10-26 10:34:45 +0200

    [IMP] Use additional_domain for skip_bank_reconciliation instead of filter

--- account_skip_bank_reconciliation/models/account_bank_statement.py
+++ account_skip_bank_reconciliation/models/account_bank_statement.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models
+from odoo.osv import expression
 
 
 class AccountBankStatementLine(models.Model):
@@ -10,10 +11,14 @@ class AccountBankStatementLine(models.Model):
     def get_move_lines_for_reconciliation(
             self, partner_id=None, excluded_ids=None, str=False, offset=0,
             limit=None, additional_domain=None, overlook_partner=False):
+        if additional_domain is None:
+            additional_domain = []
+        additional_domain = expression.AND([additional_domain, [
+            ('account_id.exclude_bank_reconcile', '=', False)
+        ]])
         am_lines = super(AccountBankStatementLine, self).\
             get_move_lines_for_reconciliation(
             partner_id=partner_id, excluded_ids=excluded_ids, str=str,
             offset=offset, limit=limit, additional_domain=additional_domain,
             overlook_partner=overlook_partner)
-        return am_lines.filtered(
-            lambda line: not line.account_id.exclude_bank_reconcile)
+        return am_lines
