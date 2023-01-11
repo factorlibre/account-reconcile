2f22f041b1f413c2f44c453f2df897345194a4de
Luis J. Salvatierra <luis.salvatierra@factorlibre.com>
2023-01-11 15:03:12 +0100

    [FIX] account_skip_bank_reconciliation: skip on reconciliation proposition

--- account_skip_bank_reconciliation/__manifest__.py
+++ account_skip_bank_reconciliation/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# Copyright 2023 FactorLibre - Luis J. Salvatierra <luis.salvatierra@factorlibre.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Account Skip Bank Reconciliation",
     "summary": "Allows to exclude from bank statement reconciliation "
                "all journal items of a reconcilable account",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "depends": ["account"],
     "author": "Eficent, Odoo Community Association (OCA)",
     "website": "http://www.github.com/OCA/account-reconcile",
--- account_skip_bank_reconciliation/readme/CONTRIBUTORS.rst
+++ account_skip_bank_reconciliation/readme/CONTRIBUTORS.rst
@@ -1,1 +1,3 @@
 * Miquel Raïch <miquel.raich@eficent.com>
+* Hugo Santos <hugo.santos@factorlibre.com>
+* Luis J. Salvatierra <luis.salvatierra@factorlibre.com>
