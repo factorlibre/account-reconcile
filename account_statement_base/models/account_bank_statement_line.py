5995b4ff91ecf64bc08a042e96b3c479d2e94a87
Sergio Bustamante <sergio.bustamante@factorlibre.com>
2024-05-24 13:31:56 +0200

    [IMP] account_statement_base: add smart button on view_bank_statement_form linking to journal items

--- account_statement_base/models/account_bank_statement_line.py
+++ account_statement_base/models/account_bank_statement_line.py
@@ -1,7 +1,7 @@
 # Copyright 2024 ForgeFlow
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import _, models
 
 
 class AccountBankStatementLine(models.Model):
@@ -19,3 +19,24 @@ class AccountBankStatementLine(models.Model):
         result["views"] = [(res and res.id or False, "form")]
         result["res_id"] = self.move_id.id
         return result
+
+
+class AccountBankStatement(models.Model):
+    _inherit = "account.bank.statement"
+
+    def open_entries(self):
+        self.ensure_one()
+        return {
+            "name": _("Journal Items"),
+            "view_mode": "tree,form",
+            "res_model": "account.move.line",
+            "view_id": False,
+            "type": "ir.actions.act_window",
+            "context": {"search_default_group_by_move": 1, "expand": 1},
+            "search_view_id": self.env.ref("account.view_account_move_line_filter").id,
+            "domain": [
+                "&",
+                ("parent_state", "=", "posted"),
+                ("statement_id", "=", self.id),
+            ],
+        }
